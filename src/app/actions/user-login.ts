67f98413b1826c24a6a83aa27f0f789d0f4f2b85
wnwlals13 <wnwlals13@naver.com>
2024-10-22 13:51:12 +0900

    fix : 배포환경 테스트중...

--- src/app/actions/user-login.ts
+++ src/app/actions/user-login.ts
@@ -2,16 +2,13 @@ import { LoginFormInputs } from '@/app/login/page';
 
 export async function userLogin(data: LoginFormInputs) {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/login`,
-      {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+    const response = await fetch(`/api/auth/login`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify(data),
+    });
 
     if (!response.ok) {
       console.error('Error');
