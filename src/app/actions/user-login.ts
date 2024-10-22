7cb8065e90fbdc9bd1da47755fcf11800ac84e6b
wnwlals13 <wnwlals13@naver.com>
2024-10-22 11:57:42 +0900

    fix : 배포환경 route 테스트중

--- src/app/actions/user-login.ts
+++ src/app/actions/user-login.ts
@@ -1,21 +1,14 @@
-'use server';
-
 import { LoginFormInputs } from '@/app/login/page';
-import { cookies } from 'next/headers';
 
 export async function userLogin(data: LoginFormInputs) {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/login`,
-      {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `${cookies().get('token')}`,
-        },
-        body: JSON.stringify(data),
+    const response = await fetch(`/api/auth/login`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify(data),
+    });
 
     if (!response.ok) {
       console.error('Error');
