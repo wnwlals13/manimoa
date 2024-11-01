bb2820feb5e082667f5d5bd036000acae8dfdc50
wnwlals13 <wnwlals13@naver.com>
2024-11-01 13:44:09 +0900

    test:배포 마이페이지 테스트 진행중

---
+++ src/app/(with-nav)/mypage/(with-nav)/@userFeeds/page.tsx
@@ -0,0 +1,37 @@
+import MyFeedItem from '@/components/feed/my-feed-item';
+import { FeedData } from '@/types';
+import { cookies } from 'next/headers';
+
+const getMyFeeds = async () => {
+  const cookieStore = cookies().get('user')?.value as string;
+  const user = JSON.parse(cookieStore);
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/getFeeds?userId=${user?.uid}`,
+    {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  if (!response.ok) {
+    return { error: `내 피드게시글 데이터 조회에 실패했습니다.` };
+  }
+  const { feeds } = await response.json();
+  return feeds;
+};
+
+export default async function Page() {
+  const feeds = await getMyFeeds();
+  return (
+    <div className="">
+      <div className="w-full border-t"></div>
+      <h1 className="mt-4 mb-4 font-bold ">내글 보기</h1>
+      {feeds &&
+        feeds.map((item: FeedData, idx: number) => (
+          <MyFeedItem key={idx} {...item} />
+        ))}
+    </div>
+  );
+}
--- src/app/(with-nav)/mypage/(with-nav)/layout.tsx
+++ src/app/(with-nav)/mypage/(with-nav)/layout.tsx
@@ -1,14 +1,17 @@
 export default function Layout({
   children,
   userInfo,
+  userFeeds,
 }: {
   children: React.ReactNode;
   userInfo: React.ReactNode;
+  userFeeds: React.ReactNode;
 }) {
   return (
     <>
       {children}
       {userInfo}
+      {userFeeds}
     </>
   );
 }
