c6a255b13b337fab5a0dc73205eccf80fb3c752b
wnwlals13 <wnwlals13@naver.com>
2024-11-01 14:21:58 +0900

    test:배포 마이페이지 테스트 진행중(파라미터확인)

--- src/app/api/user/getFeeds/route.ts
+++ src/app/api/user/getFeeds/route.ts
@@ -1,6 +1,6 @@
 import { conn } from '@/utils/db';
 import { FieldPacket, QueryResult, RowDataPacket } from 'mysql2';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
