ac2c12b19d2c2ebff479802f609c725309242d74
wnwlals13 <wnwlals13@naver.com>
2024-11-01 13:37:10 +0900

    test: 배포 마이페이지 테스트 진행중

--- src/app/(with-nav)/mypage/(with-nav)/layout.tsx
+++ src/app/(with-nav)/mypage/(with-nav)/layout.tsx
@@ -1,17 +1,14 @@
 export default function Layout({
   children,
-  userFeeds,
   userInfo,
 }: {
   children: React.ReactNode;
-  userFeeds: React.ReactNode;
   userInfo: React.ReactNode;
 }) {
   return (
     <>
       {children}
       {userInfo}
-      {userFeeds}
     </>
   );
 }
