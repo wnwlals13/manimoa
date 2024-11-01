24277b4f605398f9bfd96661c7891261b480fcb0
wnwlals13 <wnwlals13@naver.com>
2024-11-01 13:56:12 +0900

    tes : 배포 마이페이지 테스트 진행중

--- src/app/(with-nav)/mypage/(with-nav)/@userFeeds/page.tsx
+++ src/app/(with-nav)/mypage/(with-nav)/@userFeeds/page.tsx
@@ -19,6 +19,7 @@ const getMyFeeds = async () => {
     return { error: `내 피드게시글 데이터 조회에 실패했습니다.` };
   }
   const { feeds } = await response.json();
+  console.log('[MYPAGE] feeds => ', feeds);
   return feeds;
 };
 
@@ -26,12 +27,12 @@ export default async function Page() {
   const feeds = await getMyFeeds();
   return (
     <div className="">
-      <div className="w-full border-t"></div>
+      {/* <div className="w-full border-t"></div>
       <h1 className="mt-4 mb-4 font-bold ">내글 보기</h1>
       {feeds &&
         feeds.map((item: FeedData, idx: number) => (
           <MyFeedItem key={idx} {...item} />
-        ))}
+        ))} */}
     </div>
   );
 }
