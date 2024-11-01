5020a39bf241dd8e25ac21fcdca48abbb260e274
wnwlals13 <wnwlals13@naver.com>
2024-11-01 13:31:14 +0900

    test: 배포 마이페이지 에러 확인

--- src/app/(with-nav)/mypage/(with-nav)/layout.tsx
+++ src/app/(with-nav)/mypage/(with-nav)/layout.tsx
@@ -1,19 +1,16 @@
 export default function Layout({
   children,
   userFeeds,
-  userGoal,
   userInfo,
 }: {
   children: React.ReactNode;
   userFeeds: React.ReactNode;
-  userGoal: React.ReactNode;
   userInfo: React.ReactNode;
 }) {
   return (
     <>
       {children}
       {userInfo}
-      {userGoal}
       {userFeeds}
     </>
   );
